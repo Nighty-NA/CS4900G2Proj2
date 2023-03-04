61b2a622f07c883eca78d0621fd0237d5d1213c0
Megan Johnson <eartmeisterindigo@gmail.com>
2023-03-03 23:01:29 -0500

    Finished walking sprites, swapped columns for rows

--- Dawnseeker/src/main/java/myGame/simplefactory.java
+++ Dawnseeker/src/main/java/myGame/simplefactory.java
@@ -16,6 +16,8 @@ import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.collision.ContactID.Type;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
@@ -37,7 +39,9 @@ public class simplefactory implements EntityFactory {
     	
         return entityBuilder()
                 .type(EntityType.PLAYER)
-                .viewWithBBox("PlayerCharacterDawnseeker.png")
+                .bbox(new HitBox(BoundingShape.box(64, 64)))
+                
+                //.viewWithBBox("PlayerCharacterDawnseeker.png")
                 .at(500,500)
                 .collidable()
                 .with("Helth", 3)
