3a6c7120e49a5394284d077348c0e2060c6adfb2
Nathaniel Arrowood <nathanielarrowood@gmail.com>
2023-03-04 12:16:46 -0500

    Saving copy of local repo online just in case something breaks...

---
+++ Dawnseeker/src/main/java/myGame/DawnseekerMenu.java
@@ -0,0 +1,44 @@
+package myGame;
+
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.MenuType;
+
+import javafx.beans.binding.StringBinding;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+public class DawnseekerMenu extends FXGLMenu {
+
+	public DawnseekerMenu() {
+		super(MenuType.MAIN_MENU);
+	}
+	@Override
+	protected Button createActionButton(StringBinding stringBinding, Runnable runnable) {
+		return null;
+	}
+	
+	@Override 
+	protected Button createActionButton(String s, Runnable runnable) {
+		return null;
+	}
+	
+	@Override
+	protected Node createBackground(double w, double h) {
+		return null;
+	}
+	
+	@Override
+	protected Node createProfileView(String s) {
+		return null;
+	}
+	
+	@Override
+	protected Node createTitleView(String s) {
+		return null;
+	}
+	
+	@Override
+	protected Node createVersionView(String s) {
+		return null;
+	}
+}
