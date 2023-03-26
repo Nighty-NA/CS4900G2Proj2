e60215b047239c6e79d26d4646c58c0fb6b00666
Megan Johnson <eartmeisterindigo@gmail.com>
2023-03-25 22:44:51 -0400

    Implemented coin animation

---
+++ Dawnseeker/src/main/java/animationComponent/CoinAnimationComponent.java
@@ -0,0 +1,24 @@
+package animationComponent;
+
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
+
+import javafx.util.Duration;
+
+public class CoinAnimationComponent extends Component {
+	
+	private AnimationChannel coin;
+	private AnimatedTexture texture;
+	
+	public CoinAnimationComponent(){
+		coin = new AnimationChannel(FXGL.image("FireBallProjectile.png"), 2, 16, 16, Duration.seconds(0.25), 0, 4);
+		texture = new AnimatedTexture(coin);
+	}
+	
+	public void onAdded() {
+		entity.getViewComponent().addChild(texture);
+		texture.loopAnimationChannel(coin);
+	}
+}
